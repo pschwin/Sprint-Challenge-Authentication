2807856c92223b5a1e1af9481b3cad19272c353f
Patrick <patrick.schwindt14@gmail.com>
2019-04-12 10:11:19 -0600

    Need to fix GetJokes

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt = require ('bcryptjs');
+const Users = require('../users/users-model.js');
+const secret = require('./secrets.js');
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -8,12 +12,53 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken(user) {
+  const payload = {
+    subject: user.id, // subject in payload
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: '1d'
+  };
+  return jwt.sign(payload, secret.jwtSecret, options);
+}
+
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    });
 }
 
 function login(req, res) {
   // implement user login
+  let {username, password} = req.body;
+
+  Users.findBy({username})
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)){
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username} to the FunHouse`,
+          token
+        });
+      }else{
+        res.status(401).json({message: `Credentiales No Bueno`})
+      }
+    })
+    .catch(error =>{
+      res.status(500).json(error);
+    })
 }
 
 function getJokes(req, res) {
